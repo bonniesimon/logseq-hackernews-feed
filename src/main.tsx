317526d0c06ae83e914004eb2d4ad5692d5cc482
Bonnie Simon <bonnie.developer@gmail.com>
2022-03-12 19:28:38 +0530

    Initialize project
    Modified it to the point to where it works.

--- src/main.tsx
+++ src/main.tsx
@@ -23,49 +23,10 @@ function main() {
     document.getElementById("app")
   );
 
-  function createModel() {
-    return {
-      show() {
-        logseq.showMainUI();
-      },
-    };
-  }
-
-  logseq.provideModel(createModel());
-  logseq.setMainUIInlineStyle({
-    zIndex: 11,
-  });
-
-  const openIconName = "template-plugin-open";
-
-  if (isDev) {
-    // @ts-expect-error
-    top[magicKey] = true;
-  }
-
-  logseq.provideStyle(css`
-    div[data-injected-ui=${openIconName}-${pluginId}] {
-      display: inline-flex;
-      align-items: center;
-      opacity: 0.55;
-      font-weight: 500;
-      padding: 0 5px;
-      position: relative;
-    }
-
-    div[data-injected-ui=${openIconName}-${pluginId}]:hover {
-      opacity: 0.9;
-    }
-  `);
-
-  logseq.provideUI({
-    key: openIconName,
-    path: "#search",
-    template: `
-      <a data-on-click="show"
-         style="opacity: .6; display: inline-flex;">⚙️</a>
-    `,
-  });
+	logseq.Editor.registerSlashCommand('Random sheet', async () => {
+		console.log("Wowwww it works");
+	});
+  
 }
 
 // @ts-expect-error
